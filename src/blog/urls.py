0594f5dda2b2b89ef6ba539553970b4174a31df8
lite.what <lite.what@gmail.com>
2017-02-21 23:32:22 +0100

    Changed home view from function to class

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
+	url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^articles/', include('articles.urls')),  
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic.base import TemplateView
 
 from articles.forms import ArticleForm
 
-def home(request, *args, **kwargs):
-    form = ArticleForm()
-    context = {
-        'form': form,
-        'action_url': reverse('articles:create'),
-    }
-    return render(request, 'home.html', context)
+# def home(request, *args, **kwargs):
+#     form = ArticleForm()
+#     context = {
+#         'form': form,
+#         'action_url': reverse('articles:create'),
+#     }
+#     return render(request, 'home.html', context)
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ArticleForm()
+        context['action_url'] = reverse('articles:create')
+        return context
