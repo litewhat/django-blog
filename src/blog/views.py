f784d72636a6ebfcf89b02450f8dc06c1ba63d55
lite.what <lite.what@gmail.com>
2017-02-22 13:08:09 +0100

    Modified home.html

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,6 +10,6 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = ArticleForm()
-        context['action_url'] = reverse('articles:create')
+        context['authenticated'] = self.request.user.is_authenticated()
         return context
+
